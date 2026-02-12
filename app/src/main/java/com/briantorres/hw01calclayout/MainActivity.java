268bacfeaa48228915e7fa356b01641b33eb7771
btorres561 <briantorres1861@gmail.com>
2026-02-12 10:35:42 -0500

    Simplified code
    The code has been simplified to achieve the same thing with fewer lines of code.

--- app/src/main/java/com/briantorres/hw01calclayout/MainActivity.java
+++ app/src/main/java/com/briantorres/hw01calclayout/MainActivity.java
@@ -23,16 +23,6 @@ public class MainActivity extends AppCompatActivity {
         EdgeToEdge.enable(this);
         setContentView(R.layout.main_view);
 
-        ViewGroup parent = findViewById(R.id.parentVerticalLayout);
-
-        View.OnClickListener listener = v -> {
-            Button b = (Button) v;
-            Toast.makeText(this, b.getText().toString(), Toast.LENGTH_SHORT).show();
-            Log.d("BTN_CLICK", b.getText().toString());
-        };
-
-        attachListenerToAllButtons(parent, listener);
-
 //        ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.main), (v, insets) -> {
 //            Insets systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars());
 //            v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom);
@@ -42,54 +32,20 @@ public class MainActivity extends AppCompatActivity {
 
     public void loadTableLayout(View clickedView) {
         setContentView(R.layout.secondary_view);
-        Button load = (Button) clickedView;
-        Toast.makeText(this, load.getText().toString(), Toast.LENGTH_SHORT).show();
-        Log.d("BTN_CLICK", load.getText().toString());
+        onClick(clickedView);
         ViewGroup parent = findViewById(R.id.parentTableLayout);
-
-        View.OnClickListener listener = v -> {
-            Button b = (Button) v;
-            Toast.makeText(this, b.getText().toString(), Toast.LENGTH_SHORT).show();
-            Log.d("BTN_CLICK", b.getText().toString());
-        };
-
-        attachListenerToAllButtons(parent, listener);
     }
 
     public void loadLinearLayout(View clickedView) {
         setContentView(R.layout.main_view);
-        Button load = (Button) clickedView;
-        Toast.makeText(this, load.getText().toString(), Toast.LENGTH_SHORT).show();
-        Log.d("BTN_CLICK", load.getText().toString());
+        onClick(clickedView);
         ViewGroup parent = findViewById(R.id.parentVerticalLayout);
-
-        View.OnClickListener listener = v -> {
-            Button b = (Button) v;
-            Toast.makeText(this, b.getText(), Toast.LENGTH_SHORT).show();
-            Log.d("BTN_CLICK", b.getText().toString());
-        };
-
-        attachListenerToAllButtons(parent, listener);
     }
 
-    void attachListenerToAllButtons(View view, View.OnClickListener listener) {
-        if (view instanceof Button) {
-
-            int id = view.getId();
-
-            // 🚫 skip specific buttons
-            if (id == R.id.button9 || id == R.id.button20) {
-                return;
-            }
-
-            view.setOnClickListener(listener);
-
-        } else if (view instanceof ViewGroup) {
-
-            ViewGroup group = (ViewGroup) view;
-            for (int i = 0; i < group.getChildCount(); i++) {
-                attachListenerToAllButtons(group.getChildAt(i), listener);
-            }
-        }
+    public void onClick(View v)
+    {
+        Button b = (Button) v;
+        Toast.makeText(this, b.getText().toString(), Toast.LENGTH_SHORT).show();
+        Log.d("BTN_CLICK", b.getText().toString());
     }
 }
